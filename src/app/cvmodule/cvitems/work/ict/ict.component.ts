d9f884dd75d9641915ff12623ae450dee0eac277
Conny Hageluken <Conny.Hageluken@kpnmail.nl>
2022-10-05 21:54:54 +0200

    Nieuwe baan

--- src/app/cvmodule/cvitems/work/ict/ict.component.ts
+++ src/app/cvmodule/cvitems/work/ict/ict.component.ts
@@ -8,23 +8,29 @@ import { InfoShape } from 'src/app/cvmodule/share/infotable/infotable.component'
 
 export class IctComponent {
   shapes: InfoShape[] = [
-    { image2: "assets/images/work/sligro-logo.png",
-      company:  "Sligro, Veghel",
-      period: "dec 2016 – mrt 2017",
-      remark1: "stage",
-      remark2: "(test engineer, website designer en Java ontwikkelaar)",
-      link1: "sligrostage"},
-    { image2: "assets/images/work/sligro-logo.png",
-      company:  "Sligro, Veghel",
-      period: "jun 2017- nov 2018",
-      remark1: "Java ontwikkelaar",
-      link1: "sligro"},
+    { image2: "assets/images/work/beeyond.png",
+      company: "Beeyond bv, Eindhoven",
+      period: "sep 2022 - heden",
+      remark1: "Developer"
+    },
     { image2: "assets/images/work/qfactors_logo.gif",
       company:  "QFactors bv, Doetinchem",
       period: "aug 2019 - jan 2022",
       remark1: "Full stack Java developer",
       link1: "qfactors"
-    }
+    },
+    { image2: "assets/images/work/sligro-logo.png",
+      company:  "Sligro, Veghel",
+      period: "jun 2017- nov 2018",
+      remark1: "Java ontwikkelaar",
+      link1: "sligro"},
+    { image2: "assets/images/work/sligro-logo.png",
+      company:  "Sligro, Veghel",
+      period: "dec 2016 – mrt 2017",
+      remark1: "stage",
+      remark2: "(test engineer, website designer en Java ontwikkelaar)",
+      link1: "sligrostage"}
+    
   ];
 
   item: InfoShape = this.shapes[0];
